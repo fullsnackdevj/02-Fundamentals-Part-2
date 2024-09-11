e3847560e5b163bf7603a737acd7e3fe11a604d3
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-09-11 15:16:08 +0800

    added notes to introduction to objects + exercise

---
+++ starter/introduction_to_objects.js
@@ -0,0 +1,12 @@
+// exercise
+
+const jonas = {
+  firstName: "Jonas",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+// console.log(jonas.firstName);
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
