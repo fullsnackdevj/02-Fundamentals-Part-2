674364104e8ac4e4fd7830c7a34325ddf7e2338d
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-10 14:18:15 +0800

    added solution to #2 challenge

--- starter/learning_reduce_method.js
+++ starter/learning_reduce_method.js
@@ -14,11 +14,34 @@ Expected Output: 34
 
 // solution:
 
-const numbers = [2, 5, 8, 12, 7];
-const sumOfAllNumbers = numbers.reduce(sum);
+// const numbers = [2, 5, 8, 12, 7];
+// const sumOfAllNumbers = numbers.reduce(sum);
 
-function sum(prevNumber, nextNumber) {
-  return prevNumber + nextNumber;
+// function sum(prevNumber, nextNumber) {
+//   return prevNumber + nextNumber;
+// }
+
+// // console.log(sumOfAllNumbers);
+
+/*
+
+2. Product of All Numbers
+Problem: Given an array of numbers, return the product of all the numbers (multiply them all).
+
+Example Input: [3, 5, 2]
+Expected Output: 30
+
+Hint: Start the reduce() with an initial value of 1 and multiply each element.
+
+*/
+
+// solution
+
+const numbers = [3, 5, 2];
+const product = numbers.reduce(calcProduct);
+
+function calcProduct(prevNumber, nextNumber) {
+  return prevNumber * nextNumber;
 }
 
-console.log(sumOfAllNumbers);
+console.log(product);
